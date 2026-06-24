07f97e7990648a23a0d2b140d1f8e59cce7aee12
João Vitor Diniz Alves <diniz.vitor.alves@gmail.com>
2026-06-24 17:31:07 -0300

    :sparkles: Cadastro - Contas

--- conta_bancaria/src/conta_bancaria/MenuPrincipal.java
+++ conta_bancaria/src/conta_bancaria/MenuPrincipal.java
@@ -8,14 +8,16 @@ import conta_bancaria.model.ContaPoupanca;
 import conta_bancaria.util.Cores;
 
 public class MenuPrincipal {
-	
+
 	private static final Scanner leia = new Scanner(System.in);
 	private static final ContaController contaController = new ContaController();
 
 	public static void main(String[] args) {
-		
-		int opcao; 
-		
+
+		criarContasTestes();
+
+		int opcao;
+
 		do {
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND);
 			System.out.println("---------------------------------------------------------------");
@@ -33,68 +35,121 @@ public class MenuPrincipal {
 			System.out.println("_______________________________________________________________");
 			System.out.println(" Ente com a opção desejada:                                    ");
 			System.out.println("                                                               " + Cores.TEXT_RESET);
-			opcao = leia.nextInt();
-			
-			switch (opcao) {
-				case 1:
-					leia.nextLine();
-					System.out.print(" ---- Criar conta ----");
-					break;
-				case 2:
-					leia.nextLine();
-					System.out.print(" ---- Listar todas as contas ----");
-					
-					break;
-				case 3:
-					leia.nextLine();
-					System.out.print(" ---- Buscar Contas por Números ----");
-					break;
-				case 4:
-					leia.nextLine();
-					System.out.print(" ---- Atulizar dados da Conta ----");
-					break;
-				case 5:
-					leia.nextLine();
-					System.out.print(" ---- Apagar conta ----");
-					break;
-				case 6:
-					leia.nextLine();
-					System.out.print(" ---- Sacar ----");
-					break;
-				case 7:
-					leia.nextLine();
-					System.out.print(" ---- Depositar ----");
-					break;
-				case 8:
-					leia.nextLine();
-					System.out.print(" ---- Transferência ----");
-					break;
-				case 9:
-					leia.nextLine();
-					System.out.print(" ---- Sair da operação ----");
-					System.out.println("Você saiu do programa! \n\nVolte sempre." );
-					break;
-					default:
-						System.out.println("Opção Iválida!");
-		}
-		
-		} 
-		while (opcao !=9);
-	}
-	
-		public static void criarContasTestes() {
-			contaController.cadastrar(new ContaCorrente(contaController.gerarNumero(), 456, 1, "Thuany Silva", 1000000.00f, 100000.00f));
-			contaController.cadastrar(new ContaPoupanca(contaController.gerarNumero(), 456, 2, "Marcia Condarco", 1000000.00f, 10));
-		}
-		public static void listarContas() {
-			contaController.listarTodas();
-		}
-		{
-		
-		leia.close();
 
-		
+			opcao = leia.nextInt();
+
+			switch (opcao) {
+			case 1:
+
+				leia.nextLine();
+				System.out.print(" ---- Criar conta ----");
+				cadastrarContas();
+				keyPress();
+				break;
+			case 2:
+
+				leia.nextLine();
+				System.out.print(" ---- Listar todas as contas ----");
+				listarContas();
+				keyPress();
+				break;
+			case 3:
+
+				leia.nextLine();
+				System.out.print(" ---- Buscar Contas por Números ----");
+				keyPress();
+				break;
+			case 4:
+
+				leia.nextLine();
+				System.out.print(" ---- Atulizar dados da Conta ----");
+				keyPress();
+				break;
+			case 5:
+
+				leia.nextLine();
+				System.out.print(" ---- Apagar conta ----");
+				keyPress();
+				break;
+			case 6:
+
+				leia.nextLine();
+				System.out.print(" ---- Sacar ----");
+				keyPress();
+				break;
+			case 7:
+
+				leia.nextLine();
+				System.out.print(" ---- Depositar ----");
+				keyPress();
+				break;
+			case 8:
+
+				leia.nextLine();
+				System.out.print(" ---- Transferência ----");
+				keyPress();
+				break;
+			case 9:
+
+				leia.nextLine();
+				System.out.print(" ");
+				System.out.println("Você saiu do programa! \n\nVolte sempre.");
+				keyPress();
+				break;
+			default:
+				System.out.println("Opção Iválida!");
+			}
+
+		} while (opcao != 9);
+	}
+
+	public static void criarContasTestes() {
+		contaController.cadastrar(
+				new ContaCorrente(contaController.gerarNumero(), 456, 1, "Thuany Silva", 1000000.00f, 100000.00f));
+		contaController.cadastrar(
+				new ContaPoupanca(contaController.gerarNumero(), 456, 2, "Marcia Condarco", 1000000.00f, 10));
+	}
+
+	public static void keyPress() {
+		System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para continuar...");
+		leia.nextLine();
+	}
+
+	public static void listarContas() {
+		contaController.listarTodas();
+	}
+
+	public static void cadastrarContas() {
+		System.out.println("\nDigite o número da agencia: ");
+		int agencia = leia.nextInt();
+
+		System.out.println("\nDigite o nome do títular da conta: ");
+		leia.skip("\\R");
+		String titular = leia.nextLine();
+
+		System.out.println("\nDigite o tipo da conta (1 - CC | 2 - CP): ");
+		int tipo = leia.nextInt();
+
+		System.out.println("\nDigite o saldo da conta: ");
+		float saldo = leia.nextFloat();
+
+		switch (tipo) {
+		case 1 -> {
+			System.out.println("Digite o limite da conta: ");
+			float limite = leia.nextFloat();
+			leia.nextLine();
+			contaController
+					.cadastrar(new ContaCorrente(contaController.gerarNumero(), agencia, tipo, titular, saldo, limite));
+		}
+		case 2 -> {
+			System.out.println("Digite o dia do aniversário da conta: ");
+			int aniversario = leia.nextInt();
+			leia.nextLine();
+			contaController.cadastrar(
+					new ContaPoupanca(contaController.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+		}
+		default -> System.out.println(Cores.TEXT_RED + "Tipo de conta inválida" + Cores.TEXT_RESET);
+		}
 	}
 
 }
-
